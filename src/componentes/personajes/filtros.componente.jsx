7b1d3ea2ac3bea0fdb0ebcc4ecd817983bbf4087
Maite Lopez <lopezmaitev@gmail.com>
2022-04-16 17:39:21 -0300

    ADD funcionalidad al componente filtro

--- src/componentes/personajes/filtros.componente.jsx
+++ src/componentes/personajes/filtros.componente.jsx
@@ -1,10 +1,16 @@
+import { useDispatch } from 'react-redux';
 import './filtros.css';
 
 const Filtros = () => {
+    const dispatch = useDispatch();
 
     return <div className="filtros">
-        <label for="nombre">Filtrar por nombre:</label>
-        <input type="text" placeholder="Rick, Morty, Beth, Alien, ...etc" name="nombre" />
+        <div>
+            <label for="nombre">Filtrar por nombre:</label>
+            <input type="text" onChange={(e)=> distpach(buscarPersonajes(e.target.value))} 
+                   placeholder="Rick, Morty, Beth, Alien, ...etc" name="nombre" autoFocus={true}/>
+        </div>
+        
     </div>
 }
 
